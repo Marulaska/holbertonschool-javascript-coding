a5b7ae6999003db5068f3a32b056019d240f1128
Mariana Echeto <echetomariana@gmail.com>
2024-01-28 17:40:44 -0300

    completed tasks

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2] + '?completed=true';
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const arr = [];
+  const tasks = JSON.parse(body);
+  tasks.forEach(function (task) {
+    if (arr[task.userId - 1] == null) {
+      arr[task.userId - 1] = 0;
+    }
+    arr[task.userId - 1] += 1;
+  });
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[i + 1] = arr[i];
+  }
+  console.log(obj);
+});
